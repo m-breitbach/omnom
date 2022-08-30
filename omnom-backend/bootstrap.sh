198392b881f478d2c02b2dbd0bc1e819401118f1
m-breitbach <44359158+m-breitbach@users.noreply.github.com>
2022-08-30 18:25:18 +0200

    Delete omnom-backend directory

--- omnom-backend/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export FLASK_APP=./src/index.py
-source $(pipenv --venv)/bin/activate
-flask run --host 0.0.0.0 --port=80
-
--- omnom-backend/src/index.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello_world():
-  return "Hello, this is OmNom 0.1!"
